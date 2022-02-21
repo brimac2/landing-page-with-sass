ec5c64e1b76fa12d95cc7406b2170b9304e9a90e
Martynas Petruska <ixl489@c02wh0v9htdf.lan>
2022-02-21 21:21:52 +0200

    fix: bootstrap scripts

--- components/header.js
+++ components/header.js
@@ -9,43 +9,42 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <header class="nav-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-3">
-          <img
-            class="logo"
-            src="assets/KFS-DRK.png"
-            alt="Kick Flip Studios Image"
-          />
-        </div>
-        <div class="col-9">
-          <div class="nav-menu">
-            <ul class="nav-menu-list">
-              <li class="nav-item">
-                <a class="nav-a" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-a" href="about.html">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-a" href="">Portfolio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-a" href="">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-a" href="">Faq</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-a" href="">Contact</a>
-              </li>
-            </ul>
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <img
+              class="logo"
+              src="assets/KFS-DRK.png"
+              alt="Kick Flip Studios Image"
+            />
+          </div>
+          <div class="col-9">
+            <div class="nav-menu">
+              <ul class="nav-menu-list">
+                <li class="nav-item">
+                  <a class="nav-a" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-a" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-a" href="">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-a" href="">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-a" href="">Faq</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-a" href="">Contact</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </header>
-      `;
+    </header>`;
   }
 }
 
